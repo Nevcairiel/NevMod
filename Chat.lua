92caf64640c37bb69fe55e887624ce70c6627745
Hendrik Leppkes <h.leppkes@gmail.com>
2009-03-31 15:39:49 +0200

    Add Emblem of Conquest (Ulduar) to the hideLoot list.

--- Chat.lua
+++ Chat.lua
@@ -67,6 +67,7 @@ local urlPatterns = {
 local hideLoot = { 
 	[40752] = true, -- Emblem of Heroism
 	[40753] = true, -- Emblem of Valor
+	[45624] = true, -- Emblem of Conquest
 	[43228] = true, -- Stone Keeper's Shard
 }
 
