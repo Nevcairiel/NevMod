b9911cfac385d3f95ba2d96da5e69bcf1f72b706
Hendrik Leppkes <h.leppkes@gmail.com>
2014-09-28 12:14:49 +0200

    Remove sRF addin

--- NevMod.lua
+++ NevMod.lua
@@ -27,11 +27,6 @@ function NevMod:OnEnable()
 	for i=1,4 do
 		_G["GroupLootFrame"..i]:SetScale(1.2)
 	end
-
-	if sRaidFrames then
-		sRaidFrames.statusSpellTable[GetSpellInfo(774)] = 774
-		sRaidFrames:AddStatusMap("Buff_774", 50, {"indicator-tr"}, GetSpellInfo(774), {r=1, g=1, b=0, a=1}, false, {playerOnly = true})
-	end
 end
 
 -- From SmoothDurability
