c3cb768e7e2bedb1f3b7190b96f0d8a879167d90
Hendrik Leppkes <h.leppkes@gmail.com>
2016-04-29 10:50:15 +0200

    Add commented out block for SendAddonMessage debugging

--- NevMod.lua
+++ NevMod.lua
@@ -143,3 +143,13 @@ function NevMod:UpdateTracking()
 		ToggleTargetTracking(false)
 	end
 end
+
+--[[
+local SendAddonMessage_o = SendAddonMessage
+SendAddonMessage = function(prefix, text, distr, ...)
+	if distr == "RAID" and IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+		geterrorhandler()(debugstack())
+	end
+	return SendAddonMessage_o(prefix, text, distr, ...)
+end
+--]]
