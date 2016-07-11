1b4cb66a573ad208a28a8d56b9f5e9374aa54d2b
Hendrik Leppkes <h.leppkes@gmail.com>
2016-07-11 20:07:03 +0200

    Hide OrderHall CommandBar

--- NevMod.lua
+++ NevMod.lua
@@ -7,6 +7,17 @@ end
 
 function NevMod:OnInitialize()
 	self:FixBuffPositions()
+
+	-- hide the orderhall commandbar
+	hooksecurefunc("OrderHall_LoadUI",
+		function()
+			if OrderHallCommandBar then
+				OrderHallCommandBar:SetScript("OnShow", nil)
+				OrderHallCommandBar:SetScript("OnHide", nil)
+				OrderHallCommandBar:UnregisterAllEvents()
+			end
+		end)
+	hooksecurefunc("OrderHall_CheckCommandBar", function() if OrderHallCommandBar then OrderHallCommandBar:Hide() end end)
 end
 
 function NevMod:FixBuffPositions()
