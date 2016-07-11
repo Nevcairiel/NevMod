78b0726e2b24f9ee6b3ac468676691a5da3d8573
Hendrik Leppkes <h.leppkes@gmail.com>
2016-07-11 20:11:29 +0200

    Fix some buff moving

--- NevMod.lua
+++ NevMod.lua
@@ -24,7 +24,7 @@ function NevMod:FixBuffPositions()
 	BuffFrame:ClearAllPoints()
 	BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -45, -30)
 	BuffFrame.__SetPoint = BuffFrame.SetPoint
-	hooksecurefunc(BuffFrame, "SetPoint", function() BuffFrame:__SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -45, -30) end)
+	hooksecurefunc(BuffFrame, "SetPoint", function() BuffFrame:ClearAllPoints() BuffFrame:__SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -45, -30) end)
 	BuffFrame:SetFrameStrata("MEDIUM")
 end
 
