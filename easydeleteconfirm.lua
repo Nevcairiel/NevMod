be431383a3ee82ecf1feb848bd37e80088103753
Hendrik Leppkes <h.leppkes@gmail.com>
2016-07-31 23:39:19 +0200

    Add EasyDeleteConfirm module

---
+++ easydeleteconfirm.lua
@@ -0,0 +1,26 @@
+-- automatically repair and sell trash when visiting a vendor
+local EasyDeleteConfirm = LibStub("AceAddon-3.0"):GetAddon("NevMod"):NewModule("EasyDeleteConfirm", "AceEvent-3.0")
+
+function EasyDeleteConfirm:OnEnable()
+	self:RegisterEvent("DELETE_ITEM_CONFIRM")
+
+	-- create item link container
+	self.link = StaticPopup1:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
+	self.link:SetPoint('CENTER', StaticPopup1EditBox)
+	self.link:Hide()
+
+	StaticPopup1:HookScript('OnHide', function(frame) self.link:Hide() end)
+end
+
+
+function EasyDeleteConfirm:DELETE_ITEM_CONFIRM()
+	if StaticPopup1EditBox:IsShown() then
+		StaticPopup1EditBox:Hide()
+		StaticPopup1Button1:Enable()
+
+		local link = select(3, GetCursorInfo())
+
+		self.link:SetText(link)
+		self.link:Show()
+	end
+end
