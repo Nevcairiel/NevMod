bc4634ce6e4a931b424f13ac534b0b119e09c3c8
Hendrik Leppkes <h.leppkes@gmail.com>
2019-08-27 15:54:43 +0200

    Add vendorprice for classic

---
+++ vendorprice.lua
@@ -0,0 +1,36 @@
+if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then return end
+
+local function SetGameToolTipPrice(tt)
+	if not MerchantFrame:IsShown() then
+		local itemLink = select(2, tt:GetItem())
+		if itemLink then
+			local itemSellPrice = select(11, GetItemInfo(itemLink))
+			if itemSellPrice and itemSellPrice > 0 then
+				local container = GetMouseFocus()
+				local name = container:GetName()
+				local object = container:GetObjectType()
+				local count
+				if object == "Button" then -- ContainerFrameItem, QuestInfoItem, PaperDollItem
+					count = container.count
+				elseif object == "CheckButton" then -- MailItemButton or ActionButton
+					count = container.count or tonumber(container.Count:GetText())
+				end
+				local cost = (type(count) == "number" and count or 1) * itemSellPrice
+				SetTooltipMoney(tt, cost, nil, SELL_PRICE_TEXT)
+			end
+		end
+	end
+end
+
+local function SetItemRefToolTipPrice(tt)
+	local itemLink = select(2, tt:GetItem())
+	if itemLink then
+		local itemSellPrice = select(11, GetItemInfo(itemLink))
+		if itemSellPrice and itemSellPrice > 0 then
+			SetTooltipMoney(tt, itemSellPrice, nil, SELL_PRICE_TEXT)
+		end
+	end
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", SetGameToolTipPrice)
+ItemRefTooltip:HookScript("OnTooltipSetItem", SetItemRefToolTipPrice)
