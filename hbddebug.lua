d1f6f20424a855d61779c33be53bea6b8c260f2c
Hendrik Leppkes <h.leppkes@gmail.com>
2019-11-04 12:07:59 +0100

    More HBD debug

--- hbddebug.lua
+++ hbddebug.lua
@@ -9,7 +9,7 @@ if HBD then
 		local x,y,instance = HBD:GetPlayerWorldPosition()
 		local zone = HBD:GetPlayerZone()
 		if instance and HBD.mapData[zone] and HBD.mapData[zone].instance ~= instance and zone ~= -1 and not warned[instance] then
-			print(format("HereBeDragons-1.0: Instance ID %d does not match %d for zone %d", instance, HBD.mapData[zone].instance, zone))
+			print(format("HereBeDragons-2.0: Instance ID %d does not match %d for zone %d", instance, HBD.mapData[zone].instance, zone))
 			warned[instance] = true
 		end
 	end
@@ -23,4 +23,6 @@ if HBD then
 	end
 	)
 	HBD.RegisterCallback("NevMod", "PlayerZoneChanged", function(_, zone, mapType) print(format("NevMod: Location changed to %d %d (%s)", zone, mapType, HBD:GetLocalizedMap(zone) or "")) checkInstanceId() end)
+
+	HBD.___DIIDO = setmetatable(HBD.___DIIDO, { __newindex = function(t, k, v) rawset(t, k, v); print(format("HBD: Found a new dynamic instance ID override %d -> %d, in zone %d", k, v, (HBD:GetPlayerZone()))) end })
 end
