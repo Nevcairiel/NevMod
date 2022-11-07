795191260d4decb871d2cbf8ac35631ba9b10dfb
Hendrik Leppkes <h.leppkes@gmail.com>
2022-11-07 17:08:02 +0100

    Better checks for minimap stuff

--- minimapbanners.lua
+++ minimapbanners.lua
@@ -1,8 +1,13 @@
 -- move minimap banners
-if not MiniMapInstanceDifficulty then return end
-MiniMapInstanceDifficulty:ClearAllPoints()
-MiniMapInstanceDifficulty:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
-GuildInstanceDifficulty:ClearAllPoints()
-GuildInstanceDifficulty:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
-MiniMapChallengeMode:ClearAllPoints()
-MiniMapChallengeMode:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
+if MiniMapInstanceDifficulty then
+	MiniMapInstanceDifficulty:ClearAllPoints()
+	MiniMapInstanceDifficulty:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
+end
+if GuildInstanceDifficulty then
+	GuildInstanceDifficulty:ClearAllPoints()
+	GuildInstanceDifficulty:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
+end
+if MiniMapChallengeMode then
+	MiniMapChallengeMode:ClearAllPoints()
+	MiniMapChallengeMode:SetPoint("BOTTOMRIGHT", Minimap, "TOPRIGHT", 8, 10)
+end
