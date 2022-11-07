3289f0f8aa9737955d0097f0367d5834ab521b9d
Hendrik Leppkes <h.leppkes@gmail.com>
2022-11-07 12:21:04 +0100

    Disable Buff Move on retail in favor of Edit Mode

--- movebuffs.lua
+++ movebuffs.lua
@@ -1,5 +1,8 @@
 -- move the buff frame
 
+-- on retail sue edit mode
+if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then return end
+
 BuffFrame:ClearAllPoints()
 BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -45, -30)
 BuffFrame.__SetPoint = BuffFrame.SetPoint
